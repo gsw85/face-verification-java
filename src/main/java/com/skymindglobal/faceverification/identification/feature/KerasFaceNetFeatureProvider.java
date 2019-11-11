da780564a3fc00df9765bae4f72e460089b59a16
peirkern-skymind <peirkern.chuah@skymind.my>
2019-11-11 20:10:30 +0800

    Test KerasFaceNetFeatureProvider

--- src/main/java/com/skymindglobal/faceverification/identification/feature/KerasFaceNetFeatureProvider.java
+++ src/main/java/com/skymindglobal/faceverification/identification/feature/KerasFaceNetFeatureProvider.java
@@ -43,95 +43,82 @@ public class KerasFaceNetFeatureProvider extends FaceFeatureProvider {
     }
 
     public ArrayList<LabelFeaturePair> setupAnchor(File dictionary) throws IOException, ClassNotFoundException {
-        ImageRecordReader recordReader = new ImageRecordReader(224, 224, 3, new ParentPathLabelGenerator());
-        recordReader.initialize(new FileSplit(dictionary));
-        RecordReaderDataSetIterator iter = new RecordReaderDataSetIterator(recordReader, 1, 1, dictionary.listFiles().length);
-        List<String> labels = iter.getLabels();
-        generateEmbeddings(iter, labels);
-        return labelFeaturePairList;
+//        ImageRecordReader recordReader = new ImageRecordReader(224, 224, 3, new ParentPathLabelGenerator());
+//        recordReader.initialize(new FileSplit(dictionary));
+//        RecordReaderDataSetIterator iter = new RecordReaderDataSetIterator(recordReader, 1, 1, dictionary.listFiles().length);
+//        List<String> labels = iter.getLabels();
+//        generateEmbeddings(iter, labels);
+//        return labelFeaturePairList;
+        return null;
     }
 
     private void generateEmbeddings(RecordReaderDataSetIterator iter, List<String> labels) {
-        while (iter.hasNext()) {
-            DataSet Ds = iter.next();
-            INDArray embedding = this.getEmbeddings(Ds.getFeatures());
-            String label = labels.get(decodeLabelID(Ds.getLabels()));
-            labelFeaturePairList.add(new LabelFeaturePair(label, embedding));
-        }
+//        while (iter.hasNext()) {
+//            DataSet Ds = iter.next();
+//            INDArray embedding = this.getEmbeddings(Ds.getFeatures());
+//            String label = labels.get(decodeLabelID(Ds.getLabels()));
+//            labelFeaturePairList.add(new LabelFeaturePair(label, embedding));
+//        }
     }
 
     public INDArray getEmbeddings(INDArray arr) {
-        VGG16ImagePreProcessor _VGG16ImagePreProcessor = new VGG16ImagePreProcessor();
-        _VGG16ImagePreProcessor.preProcess(arr);
+//        VGG16ImagePreProcessor _VGG16ImagePreProcessor = new VGG16ImagePreProcessor();
+//        _VGG16ImagePreProcessor.preProcess(arr);
         return null; //model.feedForward(arr, false).get("fc8");
     }
 
     public static IntStream reverseOrderStream(IntStream intStream) {
-        int[] tempArray = intStream.toArray();
-        return IntStream.range(1, tempArray.length + 1).boxed()
-                .mapToInt(i -> tempArray[tempArray.length - i]);
+//        int[] tempArray = intStream.toArray();
+//        return IntStream.range(1, tempArray.length + 1).boxed()
+//                .mapToInt(i -> tempArray[tempArray.length - i]);
+        return null;
     }
 
     public List<Prediction> predict(Mat image, FaceLocalization faceLocalization, int numPredictions, double threshold, int numSamples) throws IOException {
-        NativeImageLoader nativeImageLoader = new NativeImageLoader();
-        resize(image, image, new Size(224, 224));
-        INDArray _image = nativeImageLoader.asMatrix(image);
-        INDArray anchor = getEmbeddings(_image);
-        List<Prediction> predicted = new ArrayList<>();
-        for (LabelFeaturePair i: labelFeaturePairList){
-            INDArray embed = i.getEmbedding();
-            double distance = cosineSim(anchor, embed);
-            predicted.add(new Prediction(i.getLabel(), distance, faceLocalization));
-        }
-
-        // aggregator - average comparison per class
-        List<Prediction> summary = new ArrayList<>();
-        final Map<String, List<Prediction>> map = predicted.stream().collect(Collectors.groupingBy(p -> p.getLabel()));
-        for (final Map.Entry<String, List<Prediction>> entry : map.entrySet()) {
-//            final double max = entry.getValue().stream()
-//                    .mapToDouble(p -> p.getScore()).max().getAsDouble();
-
-            double topNAvg = reverseOrderStream(entry
-                    .getValue()
-                    .stream()
-                    .mapToInt(p -> (int) (p.getScore() * 10000))
-                    .sorted()
-            )
-                    .limit(numSamples)
-                    .mapToDouble(num -> (double) num / 10000)
-                    .average()
-                    .getAsDouble();
-            if(topNAvg >= threshold) {
-                summary.add(new Prediction(entry.getKey(), topNAvg, faceLocalization));
-            }
-
-            // median
-//            MedianFinder _MedianFinder = new MedianFinder();
-//            entry.getValue().stream().forEach(p -> _MedianFinder.addNum(p.getScore()));
-//            if(_MedianFinder.findMedian() >= threshold) {
-//                summary.add(new Prediction(entry.getKey(), _MedianFinder.findMedian(), faceLocalization));
+//        NativeImageLoader nativeImageLoader = new NativeImageLoader();
+//        resize(image, image, new Size(224, 224));
+//        INDArray _image = nativeImageLoader.asMatrix(image);
+//        INDArray anchor = getEmbeddings(_image);
+//        List<Prediction> predicted = new ArrayList<>();
+//        for (LabelFeaturePair i: labelFeaturePairList){
+//            INDArray embed = i.getEmbedding();
+//            double distance = cosineSim(anchor, embed);
+//            predicted.add(new Prediction(i.getLabel(), distance, faceLocalization));
+//        }
+//
+//        // aggregator - average comparison per class
+//        List<Prediction> summary = new ArrayList<>();
+//        final Map<String, List<Prediction>> map = predicted.stream().collect(Collectors.groupingBy(p -> p.getLabel()));
+//        for (final Map.Entry<String, List<Prediction>> entry : map.entrySet()) {
+////            final double max = entry.getValue().stream()
+////                    .mapToDouble(p -> p.getScore()).max().getAsDouble();
+//
+//            double topNAvg = reverseOrderStream(entry
+//                    .getValue()
+//                    .stream()
+//                    .mapToInt(p -> (int) (p.getScore() * 10000))
+//                    .sorted()
+//            )
+//                    .limit(numSamples)
+//                    .mapToDouble(num -> (double) num / 10000)
+//                    .average()
+//                    .getAsDouble();
+//            if(topNAvg >= threshold) {
+//                summary.add(new Prediction(entry.getKey(), topNAvg, faceLocalization));
 //            }
-
-            // troubleshooting
-//            IntStream test = reverseOrderStream(entry.getValue().stream().mapToInt(p -> (int) (p.getScore() * 10000)).sorted());
-//            System.out.println("records");
-//            test.forEach(x->System.out.println(x));
-//            test = reverseOrderStream(entry.getValue().stream().mapToInt(p -> (int) (p.getScore() * 10000)).sorted()).limit(numSamples);
-//            System.out.println("limits");
-//            test.forEach(x->System.out.println(x));
-//            log.info(String.valueOf(topNAvg));
-        }
-
-        // sort and select top N
-        summary.sort(Comparator.comparing(Prediction::getScore));
-        Collections.reverse(summary);
-
-        List<Prediction> result = new ArrayList();
-        for(int i=0; i<numPredictions; i++){
-            if(i<summary.size()) {
-                result.add(summary.get(i));
-            }
-        }
-        return result;
+//        }
+//
+//        // sort and select top N
+//        summary.sort(Comparator.comparing(Prediction::getScore));
+//        Collections.reverse(summary);
+//
+//        List<Prediction> result = new ArrayList();
+//        for(int i=0; i<numPredictions; i++){
+//            if(i<summary.size()) {
+//                result.add(summary.get(i));
+//            }
+//        }
+//        return result;
+        return null;
     }
 }
